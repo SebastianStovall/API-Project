f8867ce57393644d75dd1dd6926f844d58c4482b
SebastianStovall <sebastianstovall@gmail.com>
2023-06-30 13:50:29 -0500

    added dropdown menu to ProfileButton component with css show/hide

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,32 +1,55 @@
 // frontend/src/components/Navigation/ProfileButton.js
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
+import './ProfileButton.css'
 
 function ProfileButton({ user }) {
     const dispatch = useDispatch();
+    const [showMenu, setShowMenu] = useState(false); // dropdown initially set to false
+    const ulRef = useRef(); // this hook is to capture user click on DOM
+
+
+    const openMenu = () => { // open menu when clicked
+        if (showMenu) return;
+        setShowMenu(true);
+    };
+
+
+    useEffect(() => {
+        if (!showMenu) return;
+
+        const closeMenu = (e) => {
+            if (!ulRef.current.contains(e.target)) { // close only if the user click was NOT inside of dropdown menu
+                setShowMenu(false);
+            }
+        };
+        document.addEventListener('click', closeMenu);
+        return () => document.removeEventListener("click", closeMenu);
+    }, [showMenu]);
+
 
     const logout = (e) => {
     e.preventDefault();
     dispatch(sessionActions.logout());
     };
 
-    const ulClassName = "profile-dropdown";
+    const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
 
     return (
-    <>
-        <button>
-            <i className="fas fa-user-circle" />
-        </button>
-        <ul className="profile-dropdown">
-            <li>{user.username}</li>
-            <li>{user.firstName} {user.lastName}</li>
-            <li>{user.email}</li>
-            <li>
-            <button onClick={logout}>Log Out</button>
-            </li>
-        </ul>
-    </>
+        <>
+            <button onClick={openMenu}>
+                <i className="fas fa-user-circle" />
+            </button>
+            <ul className={ulClassName} ref={ulRef}>
+                <li>{user.username}</li>
+                <li>{user.firstName} {user.lastName}</li>
+                <li>{user.email}</li>
+                <li>
+                <button onClick={logout}>Log Out</button>
+                </li>
+            </ul>
+        </>
     );
 }
 
