6b2f9eab1dc8c7eebad120aa2fb5b0899294e563
SebastianStovall <sebastianstovall@gmail.com>
2023-06-29 21:42:32 -0500

    wrap fetch requests with CSRF

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -11,6 +11,40 @@ router.use('/api', apiRouter);
 // });
 
 
+// Static routes
+// Serve React build files in production
+if (process.env.NODE_ENV === 'production') {
+    const path = require('path');
+    // Serve the frontend's index.html file at the root route
+    router.get('/', (req, res) => {
+        res.cookie('XSRF-TOKEN', req.csrfToken());
+        return res.sendFile(
+        path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+    });
+
+    // Serve the static assets in the frontend's build folder
+    router.use(express.static(path.resolve("../frontend/build")));
+
+    // Serve the frontend's index.html file at all other routes NOT starting with /api
+    router.get(/^(?!\/?api).*/, (req, res) => {
+        res.cookie('XSRF-TOKEN', req.csrfToken());
+        return res.sendFile(
+        path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+    });
+}
+
+
+// Add a XSRF-TOKEN cookie in development
+if (process.env.NODE_ENV !== 'production') {
+    router.get('/api/csrf/restore', (req, res) => {
+        res.cookie('XSRF-TOKEN', req.csrfToken());
+        return res.json({});
+    });
+}
+
+
 // Add a XSRF-TOKEN cookie... In this route, you are setting a cookie on the response with the name of XSRF-TOKEN to the value of the req.csrfToken method's return. Then, send the token as the response for easy retrieval.
 router.get("/api/csrf/restore", (req, res) => {
     const csrfToken = req.csrfToken();
