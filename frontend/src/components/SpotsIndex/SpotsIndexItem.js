f04458695790961c08907e5f96c133bbaff4e5a0
SebastianStovall <sebastianstovall@gmail.com>
2023-07-01 20:20:19 -0500

    add tooltip to each spot and css styling grid format

--- frontend/src/components/SpotsIndex/SpotsIndexItem.js
+++ frontend/src/components/SpotsIndex/SpotsIndexItem.js
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom"
 import "./SpotsIndexItem.css"
 
-export const SpotsIndexitem = ({spot}) => {
+export const SpotsIndexItem = ({spot}) => {
 
     return (
-        <Link to={`/spots/${spot.id}`} className="spot-link">
         <div id="single-spot-main-container">
-            <img className="spot-preview-img" src={spot.previewImage} alt={spot.address} />
+            <Link to={`/spots/${spot.id}`} className="spot-link" title={spot.name}>
+            {/* <img className="spot-preview-img" src={spot.previewImage} alt={spot.address} /> */}
+            <img className="spot-preview-img" src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*"/>
             <div id="single-spot-all-details-container">
                 <div id="city-price-details">
                     <p>{`${spot.city}, ${spot.state}`}</p>
@@ -14,7 +15,7 @@ export const SpotsIndexitem = ({spot}) => {
                 </div>
                 <p>{spot.avgRating ? spot.avgRating : "New"}</p>
             </div>
+            </Link>
         </div>
-        </Link>
     )
 }
--- frontend/src/components/SpotsIndex/index.js
+++ frontend/src/components/SpotsIndex/index.js
@@ -1,18 +1,15 @@
-import { SpotsIndexitem } from "./SpotsIndexItem"
+import { SpotsIndexItem } from "./SpotsIndexItem"
 import "./SpotsIndex.css"
 
 export const SpotsIndex = ({spots}) => {
 
     return (
-        <>
-            <h1>SPOTS SHOULD APPEAR UNDER HERE</h1>
             <div>
                 <div id="spots-list">
                     {spots.map((spot) => {
-                        return <SpotsIndexitem spot={spot} key={spot.id}/>
+                        return <SpotsIndexItem spot={spot} key={spot.id}/>
                     })}
                 </div>
             </div>
-        </>
     )
 }
