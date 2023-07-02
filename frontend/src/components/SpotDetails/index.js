438d9eba351fa63de3b82d8f2cbcd64341635119
SebastianStovall <sebastianstovall@gmail.com>
2023-07-02 14:02:08 -0500

    refined spotImage container css

--- frontend/src/components/SpotDetails/index.js
+++ frontend/src/components/SpotDetails/index.js
@@ -31,9 +31,11 @@ export const SpotDetails = () => {
                 {filteredSpotImages.map((img, index) => (
                     index === 0 ? ( <div className="main-img" key={img.id}> <img src="https://cdn.britannica.com/79/232779-050-6B0411D7/German-Shepherd-dog-Alsatian.jpg" alt={img.id} /> </div> ) : null
                 ))}
-                {filteredSpotImages.map((img, index) => (
-                    index !== 0 ? ( <div key={img.id} className="normal-spotImgs"> <img src="https://hips.hearstapps.com/hmg-prod/images/beautiful-smooth-haired-red-cat-lies-on-the-sofa-royalty-free-image-1678488026.jpg?crop=1xw:0.84415xh;center,top" alt={img.id} /> </div> ) : null
-                ))}
+                <div id="normal-spotImgs-container">
+                    {filteredSpotImages.map((img, index) => (
+                        index !== 0 ? ( <div key={img.id} className="normal-spotImgs"> <img src="https://hips.hearstapps.com/hmg-prod/images/beautiful-smooth-haired-red-cat-lies-on-the-sofa-royalty-free-image-1678488026.jpg?crop=1xw:0.84415xh;center,top" alt={img.id} /> </div> ) : null
+                    ))}
+                </div>
             </div>
             <div id="description-reserve-container">
             </div>
