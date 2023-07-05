5a0423ab7ee2bc50f0e7634a57fb2f03f4e7076a
SebastianStovall <sebastianstovall@gmail.com>
2023-07-05 18:15:22 -0500

    fixed refresh errors for create/delete review

--- frontend/src/components/ManageSpots/index.js
+++ frontend/src/components/ManageSpots/index.js
@@ -29,7 +29,7 @@ export const ManageSpots = () => {
                     return <div id="single-spot-main-container" key={spot.id}>
                     <Link to={`/spots/${spot.id}`} className="spot-link" title={spot.name}>
                     {/* <img className="spot-preview-img" src={spot.previewImage} alt={spot.address} /> */}
-                    <img className="spot-preview-img" src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" alt={spot.address}/>
+                    <img className="spot-preview-img" src={spot.previewImage} alt={spot.address}/>
                     <div id="single-spot-all-details-container">
                         <div id="city-price-details">
                             <p>{`${spot.city}, ${spot.state}`}</p>
