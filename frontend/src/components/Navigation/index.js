2660fd785f4534f751588d339e3845804a2661a6
SebastianStovall <sebastianstovall@gmail.com>
2023-07-05 14:02:36 -0500

    completed LoginModal css with Navigation styles when not logged in

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,5 +1,5 @@
 // frontend/src/components/Navigation/index.js
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
@@ -10,8 +10,30 @@ import "./Navigation.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCloud } from "@fortawesome/free-solid-svg-icons";
 
+
 function Navigation({ isLoaded }) {
     const sessionUser = useSelector((state) => state.session.user);
+    const [showMenu, setShowMenu] = useState(false); // dropdown initially set to false
+    const ulRef = useRef(); // this hook is to capture user click on DOM
+
+    const openMenu = () => { // open menu when clicked
+        if (showMenu) return;
+        setShowMenu(true);
+    };
+
+    useEffect(() => {
+        if (!showMenu) return;
+
+        const closeMenu = (e) => {
+            if (!ulRef.current.contains(e.target)) { // close only if the user click was NOT inside of dropdown menu
+                setShowMenu(false);
+            }
+        };
+        document.addEventListener('click', closeMenu);
+        return () => document.removeEventListener("click", closeMenu);
+    }, [showMenu]);
+
+    const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
 
     let sessionLinks;
     if (sessionUser) {
@@ -23,16 +45,28 @@ function Navigation({ isLoaded }) {
         );
     } else {
         sessionLinks = (
-        <li>
-            <OpenModalButton
-            buttonText="Log In"
-            modalComponent={<LoginFormModal />}
-        />
-        <OpenModalButton
-            buttonText="Sign Up"
-            modalComponent={<SignupFormModal />}
-        />
-        </li>
+            <>
+            <button onClick={openMenu} className="profile-button-icon">
+                <div>
+                    <div className="three-lines"></div>
+                    <div className="three-lines"></div>
+                    <div className="three-lines"></div>
+                </div>
+                <i className="fas fa-user-circle" />
+            </button>
+            <ul className={ulClassName} ref={ulRef}>
+                <div id="login-Signup-button-container">
+                    <OpenModalButton
+                        buttonText="Log In"
+                        modalComponent={<LoginFormModal />}
+                    />
+                    <OpenModalButton
+                        buttonText="Sign Up"
+                        modalComponent={<SignupFormModal />}
+                    />
+                </div>
+            </ul>
+            </>
         );
     }
 
