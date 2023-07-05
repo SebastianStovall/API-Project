8c943402ff2639088cadf15c962189c9847bf8fd
SebastianStovall <sebastianstovall@gmail.com>
2023-07-05 01:53:39 -0500

    changed static images to dynamic images

--- frontend/src/components/SpotsIndex/SpotsIndexItem.js
+++ frontend/src/components/SpotsIndex/SpotsIndexItem.js
@@ -7,8 +7,7 @@ export const SpotsIndexItem = ({spot}) => {
     return (
         <div id="single-spot-main-container">
             <Link to={`/spots/${spot.id}`} className="spot-link" title={spot.name}>
-            {/* <img className="spot-preview-img" src={spot.previewImage} alt={spot.address} /> */}
-            <img className="spot-preview-img" src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" alt={spot.address}/>
+            <img className="spot-preview-img" src={spot.previewImage} alt={spot.address}/>
             <div id="single-spot-all-details-container">
                 <div id="city-price-details">
                     <p>{`${spot.city}, ${spot.state}`}</p>
